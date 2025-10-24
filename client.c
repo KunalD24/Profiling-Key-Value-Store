7cd080ee2ca9b36915fc726c4d92c36f89611f30
Kunal <93595799+KunalD24@users.noreply.github.com>
2025-10-24 10:14:02 +0530

    Update client.c

--- client.c
+++ client.c
@@ -46,34 +46,3 @@ int main(void)
     close(fd);
     return 0;
 }
-
-// TM/Profiling/key-value-store# perf stat ./client
-// kv> SET name Kunal
-// OK  
-// kv> GET name 
-// Kunal
-// kv> ^C./client: Interrupt
-
-//  Performance counter stats for './client':
-
-//               2.67 msec task-clock
-//        #    0.000 CPUs utilized
-//                  9      context-switches
-//        #    3.365 K/sec
-//                  1      cpu-migrations
-//        #  373.846 /sec
-//                 55      page-faults
-//        #   20.562 K/sec
-//            3602260      cycles
-//        #    1.347 GHz
-//            1274745      instructions
-//        #    0.35  insn per cycle
-//             250127      branches
-//        #   93.509 M/sec
-//              13368      branch-misses
-//        #    5.34% of all branches
-
-//       11.259439636 seconds time elapsed
-
-//        0.000000000 seconds user
-//        0.019205000 seconds sys
